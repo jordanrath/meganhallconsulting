1e75ad8242a6974f17884f160ba119ebe08b65de
jordanrath <rath.jordan@icloud.com>
2022-09-19 20:31:13 -0600

    Fixed waves on section conatiners

--- src/components/section/Coaching.js
+++ src/components/section/Coaching.js
@@ -5,7 +5,7 @@ const Coaching = () => {
   return (
     <div className='coaching-info-div-container'>
       <div className='svg-coaching-info-container-top'>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 90 1440 100" preserveAspectRatio='none'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 10 1440 250" preserveAspectRatio='none'>
                 <path fill="#b56357" fillOpacity="1" d="M 0 57 L 130 35 C 262 17 474 7 720 37.3 C 960 75 1200 149 1320 186.7 L 1440 224 L 1440 320 L 1320 320 C 1200 320 960 320 720 320 C 480 320 240 320 120 320 L 0 320 Z"></path>
             </svg>
         </div>
@@ -58,8 +58,8 @@ const Coaching = () => {
         </div>
       </div>
         <div className='svg-coaching-info-container-bottom'>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 75" preserveAspectRatio='none'>
-            <path fill="#b56357" fillOpacity="1" d="M -1 33 L -1 33 C 126 -6 298 27 388 36 C 530 55 577 55 665 43 C 816 26 1068 -27 1440 44 L 1441 45 L 1440 0 L 1360 0 C 1280 0 1120 0 960 0 C 800 0 640 0 480 0 C 320 0 160 0 80 0 L 0 0 Z"></path>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 450" preserveAspectRatio='none'>
+            <path fill="#b56357" fillOpacity="1" d="M 0 0 L 120 37.3 C 240 75 480 149 720 192 C 954 225 1195 229 1289 226 L 1441 209 L 1440 0 L 1320 0 C 1200 0 960 0 720 0 C 480 0 240 0 120 0 L 0 0 Z"></path>
           </svg>
         </div>
       </div>
