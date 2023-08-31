97644171777e825ed61234a31d7bfbadecc8f2cd
jordanrath <rath.jordan@icloud.com>
2023-08-31 17:12:30 -0600

    added .env files

--- src/components/MenuOpen.js
+++ src/components/MenuOpen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Menu from './Menu';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const MenuOpen = (props) => {
     const [menuOpen, setMenuOpen] = useState(false);
