a3be568e865b7af812b6ec22fb33dd1a8b023a94
jordanrath <rath.jordan@icloud.com>
2023-08-31 16:36:38 -0600

    Updates

--- src/pages/Policies.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Policies = () => {
-  return (
-    <div>DISCLAMER PAGE</div>
-  )
-}
-
-export default Policies;
--- src/utils/funcUtils.js
+++
@@ -1,7 +0,0 @@
-//for simple, reusable funtions
-
-export const noOp = () => {};
-
-export const emptyArr = () => ([]);
-
-export const emptyObj = () => ({});
