7c02f62895914d7538b6b8e395d86429a38f9a5d
jordanrath <rath.jordan@icloud.com>
2023-09-02 16:34:12 -0600

    added inline styles to MenuOpen links

--- src/components/MenuOpen.js
+++ src/components/MenuOpen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Menu from './Menu';
 import { NavLink } from 'react-router-dom';
 
@@ -6,53 +6,77 @@ const MenuOpen = (props) => {
     const [menuOpen, setMenuOpen] = useState(false);
     const { showMenu } = props;
 
-        // Turn this into useRef as well as store julieFeld in a data file.
-        const [isDesktop, setisDesktop] = useState(window.innerWidth > 850);
-
-        const updateMediaQuery = () => {
-          setisDesktop(window.innerWidth > 850);
-        }
-      
-        useEffect(() => {
-          window.addEventListener('resize', updateMediaQuery);
-          return () => window.removeEventListener('resize', updateMediaQuery);
-        });
-
   return (
     <div className='menu'>
         <Menu onClick={() => setMenuOpen(!menuOpen)} menuOpen={menuOpen} showMenu={showMenu} />
         <div
             className='menu__overlay'
-            style={
-              isDesktop ? {
-                top: menuOpen ? '0px' : '0px',
-                left: menuOpen ? '0px' : '-1450px',
-                transitionDelay: menuOpen ? '0s' : '0s'
-              } : {
-                top: menuOpen ? '0px' : '0px',
-                left: menuOpen ? '0px' : '-900px',
+            style={{
+                top: menuOpen ? "0px" : "-105vh",
                 transitionDelay: menuOpen ? '0s' : '0s'
               }
             }
         >
             <button className='menu__buttons' onClick={() => setMenuOpen(!menuOpen)}>
-                <ul >
-                    <li>
+                <ul>
+                    <li
+                      style={{
+                        position: 'relative',
+                        left: menuOpen ? '-120px' : '0px',
+                        transform: 'translateX(120px)',
+                        transition: 'left 2s ease-in-out'
+                      }}
+                    >
                       <NavLink to='/meganhallconsulting'>Home</NavLink>
                     </li>
-                    <li>
+                    <li
+                      style={{
+                        position: 'relative',
+                        left: menuOpen ? '-120px' : '0px',
+                        transform: 'translateX(120px)',
+                        transition: 'left 1.8s ease-in-out'
+                      }}
+                    >
                       <NavLink to='/meganhallconsulting/about'>About</NavLink>
                     </li>
-                    <li>
+                    <li
+                      style={{
+                        position: 'relative',
+                        left: menuOpen ? '-120px' : '0px',
+                        transform: 'translateX(120px)',
+                        transition: 'left 1.6s ease-in-out'
+                      }}
+                    >
                       <NavLink to='/meganhallconsulting/schedule'>Work With Me</NavLink>
                     </li>
-                    <li>
+                    <li
+                      style={{
+                        position: 'relative',
+                        left: menuOpen ? '-120px' : '0px',
+                        transform: 'translateX(120px)',
+                        transition: 'left 1.4s ease-in-out'
+                      }}
+                    >
                       <NavLink to='/meganhallconsulting/blog'>Blog</NavLink>
                     </li>
-                    <li>
+                    <li
+                      style={{
+                        position: 'relative',
+                        left: menuOpen ? '-120px' : '0px',
+                        transform: 'translateX(120px)',
+                        transition: 'left 1.2s ease-in-out'
+                      }}
+                    >
                       <NavLink to='/meganhallconsulting/products'>Products</NavLink>
                     </li>
-                    <li>
+                    <li
+                      style={{
+                        position: 'relative',
+                        left: menuOpen ? '-120px' : '0px',
+                        transform: 'translateX(120px)',
+                        transition: 'left 1s ease-in-out'
+                      }}
+                    >
                       <NavLink to='/meganhallconsulting/contact'>Contact</NavLink>
                     </li>
                 </ul>
