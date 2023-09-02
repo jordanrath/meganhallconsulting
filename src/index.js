e6ee5cc91ce639b46d6fbdf8142d637c86249c33
jordanrath <rath.jordan@icloud.com>
2023-09-02 14:52:48 -0600

    created ScrollToTop component to fix issues during navigation

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './styles.css';
 import Main from './Main';
 import ThemeProvider from './contexts/ThemeProvider';
+import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider>
     <React.StrictMode>
       <BrowserRouter basename={process.env.ROUTER_BASENAME}>
+        <ScrollToTop />
         <Main />
       </BrowserRouter>
     </React.StrictMode>
