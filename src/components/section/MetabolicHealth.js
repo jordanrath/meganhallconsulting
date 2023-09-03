263b4a09f1fe71b4c711646326496ee7cf40d2d8
jordanrath <rath.jordan@icloud.com>
2023-09-03 14:31:09 -0600

    image formatting

--- src/components/section/MetabolicHealth.js
+++ src/components/section/MetabolicHealth.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
+import imageUtil from '../../utils/imageUtil';
 
 
 const MetabolicHealth = () => {
@@ -29,7 +30,7 @@ const MetabolicHealth = () => {
         </div>
           <div className='expanding-card__container'>
             <div className={isClicked === 0 ? 'expanding-card active' : 'expanding-card'} onClick={handleClick(0)}>
-            <img alt='' src='https://images.unsplash.com/photo-1541252260730-0412e8e2108e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=748&q=80' />
+            <img alt='' src={imageUtil('/images/performance.avif')} />
               <div className='expanding-card__clicked'>
                 <div className='expanding-card__text'>
                   <span className="material-symbols-outlined performance">
@@ -41,7 +42,7 @@ const MetabolicHealth = () => {
               </div>
             </div>
             <div className={isClicked === 1 ? 'expanding-card active' : 'expanding-card'} onClick={handleClick(1)}>
-            <img alt='' src='https://images.unsplash.com/photo-1588347818036-558601350947?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80' />
+            <img alt='' src={imageUtil('/images/health.avif')} />
               <div className='expanding-card__clicked'>
                 <div className='expanding-card__text'>
                   <span className="material-symbols-outlined healthspan">
@@ -53,7 +54,7 @@ const MetabolicHealth = () => {
               </div>
             </div>
             <div className={isClicked === 2 ? 'expanding-card active' : 'expanding-card'} onClick={handleClick(2)}>
-            <img alt='' src='https://images.unsplash.com/photo-1617372607364-f55cb6fa0502?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=930&q=80' />
+            <img alt='' src={imageUtil('/images/stress.jpg')} />
               <div className='expanding-card__clicked'>
                 <div className='expanding-card__text'>
                   <span className="material-symbols-outlined longevity">
