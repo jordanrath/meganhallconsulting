bbba1c2663de37fc264428c72d0404e63bec0282
jordanrath <rath.jordan@icloud.com>
2023-09-09 09:34:09 -0600

    implemented lazy loading on remaining images

--- src/components/section/Introduction.js
+++ src/components/section/Introduction.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import imageUtil from '../../utils/imageUtil';
+import { useInView } from 'react-intersection-observer';
 
 const Introduction = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    fallbackInView: true,
+  });
 
   return (
     <div className='introduction-section'>
       <div className='introduction-container'>
-        <div className='introduction-container-content'>
-            <img src={imageUtil('/images/megan-cooper.jpg')} alt='Megan and Cooper' className='intro-img' data-aos="flip-right"/>
+        <div ref={ref} className='introduction-container-content'>
+          {inView ?
+            <img 
+              src={imageUtil('/images/megan-cooper.jpg')} 
+              alt='Megan and Cooper' 
+              className='intro-img' 
+              data-aos="flip-right"
+            />
+          :
+            null
+          }
             <div className='intro-text' data-aos="zoom-in-up">
               <h2>Hi, I'm Megan Hall</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa tincidunt nunc pulvinar sapien et ligula. Ultricies lacus sed turpis tincidunt id aliquet. Tempus urna et pharetra pharetra massa. Pellentesque massa placerat duis ultricies lacus sed turpis. Orci dapibus ultrices in iaculis nunc sed augue lacus. </p>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import imageUtil from '../utils/imageUtil';
+import { useInView } from 'react-intersection-observer';
 
-const about = () => {
+const About = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    fallbackInView: true,
+  });
+  
   return (
     <div className='about-page container'>
-      <div className='about-container'>
-      <img src={imageUtil('/images/about-page-image.jpg')} data-aos='fade-in' alt='Megan and Cooper' className='about-img'/>
+      <div ref={ref} className='about-container'>
+        {inView ? 
+          <img 
+            src={imageUtil('/images/about-page-image.jpg')} 
+            data-aos='fade-in' 
+            alt='Megan and Cooper' 
+            className='about-img'
+          />
+        :
+          null
+      }
           <div className='about-text' data-aos='fade-in'>
             <h2>Hi, I'm Megan Hall</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa tincidunt nunc pulvinar sapien et ligula. Ultricies lacus sed turpis tincidunt id aliquet. Tempus urna et pharetra pharetra massa. Pellentesque massa placerat duis ultricies lacus sed turpis. Orci dapibus ultrices in iaculis nunc sed augue lacus. </p>
@@ -16,4 +31,4 @@ const about = () => {
   )
 }
 
-export default about;
+export default About;
